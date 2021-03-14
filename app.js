9af78b311bbdf883fb538d284a40db4bc3e415d4
Alexey Shumilin <shumil.alexey@gmail.com>
2021-03-14 14:37:18 +0200

    final attent
    mongoose error?

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { errors } = require('celebrate');
 
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const { corsConfig } = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const centralErrorsHandler = require('./middlewares/centralErrorsHandler');
 const limiter = require('./middlewares/limiter');
 
-const { MONGO_URL, mongoSetting } = require('./config');
+const { MONGO_URL, mongoSettings } = require('./config');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
 app.use('*', cors(corsConfig));
 app.use(helmet());
 
 mongoose.connect(MONGO_URL, mongoSettings);
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(requestLogger);
 app.use(limiter);
--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
 
+const { JWT_SECRET = 'dev-secret', MONGO_URL = 'mongodb://localhost:27017/moviedb' } = process.env;
+
 const mongoSettings = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 };
-const { MONGO_URL = 'mongodb://localhost:27017/moviedb' } = process.env;
 
 module.exports = {
-  PORT: 3000,
-  JWT_SECRET: (process.env.NODE_ENV !== 'production') ? 'JWT_SECRET' : process.env.JWT_SECRET,
+  JWT_SECRET,
   MONGO_URL,
-  mongoSetting,
+  mongoSettings,
 };
