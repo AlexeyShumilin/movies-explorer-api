77acf8dc19b4521ef7896245908b1820fd8da5a6
Alexey Shumilin <shumil.alexey@gmail.com>
2021-03-14 06:45:12 +0200

    first rewiew mistakes
    fixed routes(POST)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
-const routerUsers = require('./users.js');
-const routerMovies = require('./movies.js');
-const NotFoundError = require('../errors/notfounderr.js');
 
-router.use(routerUsers);
-router.use(routerMovies);
+const { createUser, login } = require('../controllers/users');
+const usersRouter = require('./users');
+const moviesRouter = require('./movies');
+const NotFoundError = require('../errors/notfounderr');
+
+const { auth } = require('../middlewares/auth');
+const { checkSignup, checkSignIn } = require('../middlewares/validator');
+
+router.post('/signup', checkSignup, createUser);
+router.post('/signin', checkSignIn, login);
+
+router.use('/users', auth, usersRouter);
+router.use('/movies', auth, moviesRouter);
 router.use('*', () => {
   throw new NotFoundError('Not found');
 });
